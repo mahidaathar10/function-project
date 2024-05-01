e6b1cc18e54d386929b7ae82a2e74e68279af628
mahidaathar10 <160022747+mahidaathar10@users.noreply.github.com>
2024-05-01 21:21:19 +0500

    Add files via upload

---
+++ function.ts
@@ -0,0 +1,24 @@
+function coleagueName(){
+  console.log("MAHIDA ATHAR");
+}
+coleagueName();
+
+
+function carNames(){
+let carName= "toyota corolla";
+let carName2="civic";
+let carName3="mira";
+console.log(carName);
+console.log(carName2);
+console.log(carName3);
+} 
+carNames();
+function carPrizes(){
+  let prize=20000;
+let prize2= 30000;
+let prize3= 40000;
+let prizes=prize+prize2+prize3
+console.log(prizes)
+};
+carPrizes();
+
